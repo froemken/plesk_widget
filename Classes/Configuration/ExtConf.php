e0097dfce58f1d3fc576e9ed1ef19ab2c97f819e
Stefan Froemken <froemken@gmail.com>
2021-01-14 22:41:29 +0100

    Set credentials dynamically

---
+++ Classes/Configuration/ExtConf.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types = 1);
+
+/*
+ * This file is part of the package stefanfroemken/plesk-widget.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace StefanFroemken\PleskWidget\Configuration;
+
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/*
+ * This class streamlines all settings from extension manager
+ */
+class ExtConf implements SingletonInterface
+{
+    /**
+     * @var string
+     */
+    protected $host = '';
+
+    /**
+     * @var int
+     */
+    protected $port = 8443;
+
+    /**
+     * @var string
+     */
+    protected $username = '';
+
+    /**
+     * @var string
+     */
+    protected $password = '';
+
+    public function __construct()
+    {
+        // get global configuration
+        $extConf = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('plesk_widget');
+        if (is_array($extConf) && count($extConf)) {
+            // call setter method foreach configuration entry
+            foreach ($extConf as $key => $value) {
+                $methodName = 'set' . ucfirst($key);
+                if (method_exists($this, $methodName)) {
+                    $this->$methodName($value);
+                }
+            }
+        }
+    }
+
+    public function getHost(): string
+    {
+        return $this->host;
+    }
+
+    public function setHost(string $host): void
+    {
+        $this->host = $host;
+    }
+
+    public function getPort(): int
+    {
+        return $this->port;
+    }
+
+    public function setPort(string $port): void
+    {
+        $this->port = (int)$port;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+}
--- Classes/DataProvider/PleskDataProvider.php
+++ Classes/DataProvider/PleskDataProvider.php
@@ -3,7 +3,7 @@
 declare(strict_types = 1);
 
 /*
- * This file is part of the package stefanfroemken/mysql-widget.
+ * This file is part of the package stefanfroemken/plesk-widget.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
@@ -11,13 +11,22 @@ declare(strict_types = 1);
 
 namespace StefanFroemken\PleskWidget\DataProvider;
 
-use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
+use StefanFroemken\PleskWidget\Configuration\ExtConf;
 use TYPO3\CMS\Dashboard\WidgetApi;
 use TYPO3\CMS\Dashboard\Widgets\ChartDataProviderInterface;
 
 class PleskDataProvider implements ChartDataProviderInterface
 {
+    /**
+     * @var ExtConf
+     */
+    protected $extConf;
+
+    public function __construct(ExtConf $extConf)
+    {
+        $this->extConf = $extConf;
+    }
+
     public function getChartData(): array
     {
         return [
@@ -38,8 +47,8 @@ class PleskDataProvider implements ChartDataProviderInterface
 
     protected function getWebSpaceStatus()
     {
-        $client = new \PleskX\Api\Client('900410.jweiland-hosting.de');
-        $client->setCredentials('froemkenl', 'vfM!BiX6V2$5^cDP');
+        $client = new \PleskX\Api\Client($this->extConf->getHost(), $this->extConf->getPort());
+        $client->setCredentials($this->extConf->getUsername(), $this->extConf->getPassword());
 
         //$client->customer()->getAll();
 
@@ -65,5 +74,10 @@ class PleskDataProvider implements ChartDataProviderInterface
             2 => 40
         ];*/
         return $client->customer()->getAll();
+    }
+
+    protected function getLimits()
+    {
+        //$client->webspace()->getLimits(null, null)
     }
 }
