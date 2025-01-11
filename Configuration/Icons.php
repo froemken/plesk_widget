53848fe39134cb76acd68db0556311c5a3929539
Stefan Froemken <froemken@gmail.com>
2025-01-11 23:37:39 +0100

    Register plesk icon for dashboard widgets

---
+++ Configuration/Icons.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the package jweiland/glossary2.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+return [
+    'ext-plesk-widget-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:plesk_widget/Resources/Public/Icons/plesk.svg',
+    ],
+];
