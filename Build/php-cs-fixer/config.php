74a5100389771806ed8ad01bb89fc23d73a59ca7
Stefan Froemken <froemken@gmail.com>
2025-01-11 20:50:59 +0100

    Update CGL class header

--- Build/php-cs-fixer/config.php
+++ Build/php-cs-fixer/config.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the package jweiland/glossary2.
+ * This file is part of the package stefanfroemken/plesk-widget.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
@@ -17,7 +17,7 @@ if (PHP_SAPI !== 'cli') {
 }
 
 $headerComment = <<<COMMENT
-This file is part of the package jweiland/glossary2.
+This file is part of the package stefanfroemken/plesk-widget.
 
 For the full copyright and license information, please read the
 LICENSE file that was distributed with this source code.
