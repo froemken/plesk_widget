8fc31508401c19481a51a940c6f5ad3287bb923f
Stefan Froemken <froemken@gmail.com>
2025-01-14 20:12:09 +0100

    Implement easier ExtConf solution

--- Classes/Configuration/ExtConf.php
+++ Classes/Configuration/ExtConf.php
@@ -14,20 +14,93 @@ namespace StefanFroemken\PleskWidget\Configuration;
 /*
  * This class streamlines all settings from extension settings
  */
-readonly class ExtConf
+
+use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException;
+use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Utility\MathUtility;
+
+#[Autoconfigure(constructor: 'create')]
+final readonly class ExtConf
 {
+    private const EXT_KEY = 'plesk_widget';
+
+    private const DEFAULT_SETTINGS = [
+        'host' => '',
+        'port' => 8443,
+        'username' => '',
+        'password' => '',
+        'diskUsageType' => '%',
+        'domain' => '',
+    ];
+
     public function __construct(
-        private CredentialsConfiguration $credentialsConfiguration,
-        private ViewConfiguration $viewConfiguration
+        private string $host,
+        private int $port,
+        private string $username,
+        private string $password,
+        private DiskUsageTypeEnum $diskUsageType,
+        private string $domain,
     ) {}
 
-    public function getCredentialsConfiguration(): CredentialsConfiguration
+    public static function create(ExtensionConfiguration $extensionConfiguration): self
     {
-        return $this->credentialsConfiguration;
+        $extensionSettings = self::DEFAULT_SETTINGS;
+
+        // Overwrite default extension settings with values from EXT_CONF
+        try {
+            $extensionSettings = array_merge($extensionSettings, $extensionConfiguration->get(self::EXT_KEY));
+        } catch (ExtensionConfigurationExtensionNotConfiguredException|ExtensionConfigurationPathDoesNotExistException) {
+        }
+
+        // Make sure port is integer. Else: Use default value
+        if (!MathUtility::canBeInterpretedAsInteger($extensionSettings['port'])) {
+            $extensionSettings['port'] = self::DEFAULT_SETTINGS['port'];
+        }
+
+        // Convert value to DiskUsageType enum. If not found use default value to convert to DiskUsageType
+        if (($extensionSettings['diskUsageType'] = DiskUsageTypeEnum::tryFrom($extensionSettings['diskUsageType'])) === null) {
+            $extensionSettings['diskUsageType'] = DiskUsageTypeEnum::from(self::DEFAULT_SETTINGS['diskUsageType']);
+        }
+
+        return new self(
+            host: (string)$extensionSettings['host'],
+            port: (int)$extensionSettings['port'],
+            username: (string)$extensionSettings['username'],
+            password: (string)$extensionSettings['password'],
+            diskUsageType: $extensionSettings['diskUsageType'],
+            domain: (string)$extensionSettings['domain'],
+        );
     }
 
-    public function getViewConfiguration(): ViewConfiguration
+    public function getHost(): string
     {
-        return $this->viewConfiguration;
+        return $this->host;
+    }
+
+    public function getPort(): int
+    {
+        return $this->port;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getDiskUsageType(): DiskUsageTypeEnum
+    {
+        return $this->diskUsageType;
+    }
+
+    public function getDomain(): string
+    {
+        return $this->domain;
     }
 }
